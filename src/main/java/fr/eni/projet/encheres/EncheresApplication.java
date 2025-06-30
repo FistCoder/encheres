be5e403ca2362ffdabf7b4812fc7c8d7ee35a646
Sam3550 <Samir3550.sg@gmail.com>
2025-06-30 10:49:31 +0200

    nom

--- src/main/java/fr/eni/projet/encheres/EncheresApplication.java
+++ src/main/java/fr/eni/projet/encheres/EncheresApplication.java
@@ -8,7 +8,6 @@ public class EncheresApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EncheresApplication.class, args);
-        System.out.println("ss");
     }
 
 }
