66aee32f815c50c6f6447c4f57efe3ba3029d4eb
Sam3550 <Samir3550.sg@gmail.com>
2025-07-03 16:00:20 +0200

    exception for login

--- src/main/java/fr/eni/projet/encheres/configuration/SecurityConfig.java
+++ src/main/java/fr/eni/projet/encheres/configuration/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
         http.formLogin(form -> {
             form.loginPage("/login").permitAll();
             form.defaultSuccessUrl("/").permitAll();
+            form.failureUrl("/login-error").permitAll();
         });
 
         // /logout --> vider la session et le contexte de sécurité
--- src/main/java/fr/eni/projet/encheres/ihm/LoginController.java
+++ src/main/java/fr/eni/projet/encheres/ihm/LoginController.java
@@ -6,10 +6,7 @@ import fr.eni.projet.encheres.bo.Utilisateur;
 import fr.eni.projet.encheres.exceptions.InvalidLoginException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import static fr.eni.projet.encheres.exceptions.InvalidLoginCode.NOT_FOUND;
 
@@ -25,19 +22,10 @@ class LoginController {
 
 
     // (Samir) : Ajout de la méthode pour vérifier si l'email existe déjà
-    @PostMapping("/login")
-    public String login(@ModelAttribute Utilisateur utilisateur, Model model) {
-        try {
-            // Vérifie si l'email de l'utilisateur existe déjà via le service
-            utilisateurService.checkEmailExists(utilisateur.getEmail());
-            // Si l'email existe, on redirige vers la page de login
-            return "redirect:/index";
-        } catch (InvalidLoginException e) {
-            // Si une exception est levée (email déjà utilisé), on ajoute un message d'erreur au modèle
-            model.addAttribute("error", e.getMessage());
-            // Puis on redirige vers la page d'inscription
-            return "redirect:/register";
-        }
+    @GetMapping("/login-error")
+    public String login(Model model) {
+        model.addAttribute("error", "Email ou mot de passe incorrect");
+        return "login";
     }
 
 
